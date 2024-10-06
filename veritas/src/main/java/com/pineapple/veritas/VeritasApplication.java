6c7a50edc1734695be79022237b49840a7ddbe90
AveryYay <mf3332@columbia.edu>
2024-10-06 13:38:52 -0400

    Set up Spring

---
+++ veritas/src/main/java/com/pineapple/veritas/VeritasApplication.java
@@ -0,0 +1,13 @@
+package com.pineapple.veritas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeritasApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VeritasApplication.class, args);
+  }
+
+}
