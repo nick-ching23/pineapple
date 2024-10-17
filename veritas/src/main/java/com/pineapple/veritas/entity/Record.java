06db8c818f7deaea27b0de6155462218e944f83f
naren-loganathan <naren.spirion@gmail.com>
2024-10-17 11:07:57 -0400

    Checkstyle fixes

--- veritas/src/main/java/com/pineapple/veritas/entity/Record.java
+++ veritas/src/main/java/com/pineapple/veritas/entity/Record.java
@@ -4,10 +4,13 @@ package com.pineapple.veritas.entity;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+/**
+ * Represents a row/record in the database.
+ */
 @Data
 @TableName("records")
 public class Record {
-  private String orgID;
-  private String userID;
+  private String orgId;
+  private String userId;
   private int numFlags;
 }
--- veritas/src/main/java/com/pineapple/veritas/mapper/RecordMapper.java
+++ veritas/src/main/java/com/pineapple/veritas/mapper/RecordMapper.java
@@ -4,7 +4,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pineapple.veritas.entity.Record;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * Maps record-related operations to SQL statements.
+ */
 @Mapper
-public interface RecordMapper extends BaseMapper<Record>{
+public interface RecordMapper extends BaseMapper<Record> {
 
 }
